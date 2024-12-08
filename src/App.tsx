3702601c5310e4ec4299a093ab34a68756d1e925
BlackProgrammer-prog <aziziparham2020@gmail.com>
2024-12-09 00:11:31 +0330

    commit Third

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css';
-import './index.css';
+import "./index.css";
 import image from './assets/introduction1.jpg';
-
+import image2 from './assets/introduction2.jpg';
+import image3 from './assets/introduction3.jpg';
 function App() {
-    const text1 : string = "Let's fulfill your fashion needs with shoearight now!";
-    const textButton :string = "Get Started";
-    // eslint-disable-next-line prefer-const
-    let [color,setColor] = useState<string>("#868686");
-    const ChangeColor = ()=>{
-        setColor(color="black");
+    // const textButton :string = "Get Started";
+    const [ClickCount,SetClickCount] = useState<number>(0);
+    const [DisplayStarter,ChangeDisplayStarter] = useState<string>("block");
+    const [Paragraph,ChangeParagraph] = useState<string>("Let's fulfill your fashion needs with shoearight now!");
+    const [TextButton,ChangeTextButton] = useState<string>("Get Started");
+    const [Image,ChangeImage] = useState<string>(image);
+    const handelClick = ()=>{
+        SetClickCount((x)=>x+1);
     };
+    useEffect(()=>{
+        if (ClickCount===1){
+            ChangeParagraph("Your satisfaction is our number one periority");
+            ChangeTextButton("Next");
+            ChangeImage(image2);
+        }
+        if (ClickCount===2){
+            ChangeParagraph("We provide high quality products just for you");
+            ChangeTextButton("Next");
+            ChangeImage(image3);
+        }
+    },[ClickCount]);
+    if (ClickCount===3){
+        ChangeDisplayStarter("none");
+    }
   return (
     <>
-        <div className={"m-auto w-full h-screen pt-[10px]"} style={{backgroundColor: '#1E1E1E'}}>
-            <img src={image} alt={"image introduction1"} className={"m-auto w-[290px] h-[375px]"}/>
+        <div className={"m-auto w-full h-screen pt-[10px]"} style={{backgroundColor: '#1E1E1E',display: DisplayStarter}}>
+            <img src={Image} alt={"image introduction1"} className={"m-auto w-[290px] h-[375px]"}/>
             <div className={"m-auto w-[290px] bg-white"}>
                 <div className={"flex justify-center items-center"}>
                     <div>
-                        <div><p className={"text-center font-bold mb-[15px] mt-[10px]"}>{text1}</p></div>
+                        <div><p className={"text-center font-bold mb-[15px] mt-[10px]"}>{Paragraph}</p></div>
                         <div className={"m-auto flex justify-between w-[100px]"}>
-                            <div style={{backgroundColor: "black"}} className={"w-[30px] h-[10px] rounded-[10px]"}></div>
-                            <div style={{backgroundColor: color}} className={"w-[30px] h-[10px] rounded-[10px]"}></div>
-                            <div style={{backgroundColor: "#868686"}} className={"w-[30px] h-[10px] rounded-[10px]"}></div>
+                            <div style={{backgroundColor: ClickCount===1 ? "#868686" : ClickCount===2 ? "#868686" : "black"}} className={"w-[30px] h-[10px] rounded-[10px]"}></div>
+                            <div style={{backgroundColor: ClickCount===1 ? "black" : ClickCount===2 ? "#868686" : "#868686"}} className={"w-[30px] h-[10px] rounded-[10px]"}></div>
+                            <div style={{backgroundColor: ClickCount===2 ? "black" : "#868686"}} className={"w-[30px] h-[10px] rounded-[10px]"}></div>
                         </div>
                         <div className={"m-auto flex justify-center"}>
-                            <div className={"border-solid border-[2px] border-black rounded-[10px] text-white bg-black mb-[10px] mt-[15px] w-[235px]"} onClick={ChangeColor}><h3 className={"text-center"}>{textButton}</h3></div>
+                            <div className={"border-solid border-[2px] border-black rounded-[10px] text-white bg-black mb-[10px] mt-[15px] w-[235px]"} onClick={handelClick}><h3 className={"text-center"}>{TextButton}</h3></div>
                         </div>
                     </div>
                 </div>
@@ -37,7 +55,6 @@ function App() {
     </>
   )
 }
-
 export default App
 
 {/*<div>*/
